c2b314722043c607cdf827e5ec191c5a87f543f1
jannyHou <juehou@ca.ibm.com>
2016-11-23 14:03:18 -0500

    Cloudant doesnnt handle bad url

---
+++ test.js
@@ -0,0 +1,19 @@
+var Cloudant = require('cloudant');
+var inspect = require('util').inspect;
+var goodUrl = 'https://USERNAME:PASSWORD@example.cloudant.com';
+
+Cloudant(goodUrl, function(err, cloudant) {
+	console.log('good url test results:');
+	// Error is caught properly with well formatted url
+	if (err) return console.log(err);
+	console.log(inspect(cloudant));
+});
+
+var invalidUrl = 'invalid'; // Not parsed properly in cloudant
+Cloudant(invalidUrl, function(err, cloudant) {
+	// This code is never reached, error is not
+	// handled with the callback!
+	console.log('invalid url test results:');
+	if (err) return console.log(err);
+	console.log(inspect(cloudant));
+});
